e92c19f27417c588d6ff52c4b0a1de32451aaf17
osk2090 <osk2090@gmail.com>
2021-03-15 00:23:32 +0900

    .

---
+++ app/src/main/java/com/osk2090/pms/Client/ClientApp.java
@@ -0,0 +1,7 @@
+package com.osk2090.pms.Client;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        System.out.println("미니 프로젝트 관리 스스템");
+    }
+}
---
+++ app/src/main/java/com/osk2090/pms/Server/ServaerApp.java
@@ -0,0 +1,8 @@
+package com.osk2090.pms.Server;
+
+//데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
+public class ServaerApp {
+    public static void main(String[] args) {
+        System.out.println("파일 관리 서버");
+    }
+}
