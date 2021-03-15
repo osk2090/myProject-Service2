c6bf8387b61d92469a98fdb2387a21ad1704c62a
osk2090 <osk2090@gmail.com>
2021-03-15 13:15:25 +0900

    .

--- app/src/main/java/com/osk2090/pms/Client/ClientApp.java
+++ app/src/main/java/com/osk2090/pms/Client/ClientApp.java
@@ -1,7 +1,38 @@
 package com.osk2090.pms.Client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClientApp {
+
+    String serverAddress;
+    int port;
+
     public static void main(String[] args) {
-        System.out.println("미니 프로젝트 관리 스스템");
+        ClientApp app = new ClientApp("localhost", 8888);
+        app.execute();
+    }
+
+    public ClientApp(String serverAddress, int port) {
+        this.serverAddress = serverAddress;
+        this.port = port;
+    }
+
+    public void execute() {
+        try (Socket socket = new Socket(this.serverAddress, this.port);
+             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+             DataInputStream in = new DataInputStream(socket.getInputStream());) {
+
+            out.writeUTF("hello");
+            out.flush();
+
+            String response = in.readUTF();
+            System.out.println(response);
+
+        } catch (Exception e) {
+            System.out.println("서버와 통신하는 중에 오류 발생!");
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/osk2090/pms/Server/ServaerApp.java
+++ app/src/main/java/com/osk2090/pms/Server/ServaerApp.java
@@ -1,8 +1,49 @@
 package com.osk2090.pms.Server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 //데이터를 파일에 보관하고 꺼내는 일을 할 애플리케이션
 public class ServaerApp {
+
+    int port;
+
     public static void main(String[] args) {
-        System.out.println("파일 관리 서버");
+        ServaerApp app = new ServaerApp(8888);
+        app.service();
+    }
+
+    public ServaerApp(int port) {
+        this.port = port;
+    }
+
+    public void service() {
+        //클라이언트 연결을 기다리는 서버 소켓 생성
+        try (ServerSocket serverSocket = new ServerSocket(this.port)) {
+            System.out.println("서버 실행!");
+            processRequest(serverSocket.accept());
+
+        } catch (Exception e) {
+            System.out.println("서버 실행 중 오류 발생!");
+            e.printStackTrace();
+        }
+    }
+
+    private void processRequest(Socket socket) {
+        try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+             DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+            String request = in.readUTF();
+            System.out.println(request);
+
+            out.writeUTF("success");
+            out.flush();
+
+        } catch (Exception e) {
+            System.out.println("클라이언트의 요청을 처리하는 중에 오류 발생!");
+            e.printStackTrace();
+        }
     }
 }
