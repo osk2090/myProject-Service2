505337823410e5ae1d7e7bf2f0d60ba3434da79c
osk2090 <osk2090@naver.com>
2021-04-12 00:00:04 +0900

    improve mybatis

--- app/src/main/java/com/osk2090/pms/Client/dao/mariadb/ClientDaoImpl.java
+++ app/src/main/java/com/osk2090/pms/Client/dao/mariadb/ClientDaoImpl.java
@@ -2,86 +2,31 @@ package com.osk2090.pms.Client.dao.mariadb;
 
 import com.osk2090.pms.Client.dao.ClientDao;
 import com.osk2090.pms.Client.domain.Client;
+import org.apache.ibatis.session.SqlSession;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
 
 public class ClientDaoImpl implements ClientDao {
 
-    Connection con;
+    SqlSession sqlSession;
 
-    public ClientDaoImpl() throws Exception {
-        this.con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/servicedb?user=osk&password=2090"
-        );
+    public ClientDaoImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
     }
 
     public int insert(Client client) throws Exception {
-        try (PreparedStatement stmt = con.prepareStatement(
-                "insert into pms_client(name, phone_number, birth_number, id, cSize) " +
-                        "values (?,?,?,?,?)")) {
-            stmt.setString(1, client.getName());
-            stmt.setString(2, client.getPhone_number());
-            stmt.setString(3, client.getBirth_number());
-            stmt.setString(4, client.getId());
-            stmt.setInt(5, client.getcSize());
-
-            return stmt.executeUpdate();
-        }
+        return sqlSession.insert("ClientMapper.insert", client);
     }
 
     public Client findByNo(int no) throws Exception {
-        try (PreparedStatement stmt = con.prepareStatement(
-                "select * from pms_client where no = ?")) {
-            stmt.setInt(1, no);
-
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (!rs.next()) {
-                    return null;
-                }
-
-                Client c = new Client();
-                //이름: %s 전화번호: %s 생년월일: %s 아이디: %s 사이즈:
-                c.setNo(no);
-                c.setName(rs.getString("name"));
-                c.setPhone_number(rs.getString("phone_number"));
-                c.setBirth_number(rs.getString("birth_number"));
-                c.setId(rs.getString("id"));
-                c.setcSize(rs.getInt("cSize"));
-
-                return c;
-            }
-        }
+        return sqlSession.selectOne("ClientMapper.findByNo", no);
     }
 
     public List<Client> findAll() throws Exception {
-        ArrayList<Client> list = new ArrayList<>();
-
-        try (PreparedStatement stmt = con.prepareStatement(
-                "select no,name,id from pms_client");
-             ResultSet rs = stmt.executeQuery()) {
-
-            while (rs.next()) {
-                Client c = new Client();
-                c.setNo(rs.getInt("no"));
-                c.setName(rs.getString("name"));
-                c.setId(rs.getString("id"));
-                list.add(c);
-            }
-        }
-        return list;
+        return sqlSession.selectList("ClientMapper.findAll");
     }
 
     public int delete(int no) throws Exception {
-        try (PreparedStatement stmt = con.prepareStatement(
-                "delete from pms_client where no=?")) {
-
-            stmt.setInt(1, no);
-            return stmt.executeUpdate();
-        }
+        return sqlSession.delete("ClientMapper.delete", no);
     }
 }
