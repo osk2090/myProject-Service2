c7c516275d115e769b4fc5eaf3caef111207169a
osk2090 <osk2090@naver.com>
2021-05-07 01:15:18 +0900

    put nike logo

--- myProject-Service2-server/app/src/main/java/com/osk2090/pms/web/MenuHandler.java
+++ myProject-Service2-server/app/src/main/java/com/osk2090/pms/web/MenuHandler.java
@@ -1,10 +1,13 @@
 package com.osk2090.pms.web;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -22,6 +25,24 @@ public class MenuHandler extends HttpServlet {
         out.println("<title>Nike</title>");
         out.println("</head>");
         out.println("<body>");
+
+        ServletContext ctx = request.getServletContext();
+        String path = ctx.getRealPath("/nike.jpg");
+        System.out.println(path);
+
+        FileInputStream in = new FileInputStream(path);
+
+        response.setContentType("image/jpeg");
+        BufferedOutputStream drop = new BufferedOutputStream(response.getOutputStream());
+
+        int b;
+        while ((b = in.read()) != -1) {
+            drop.write(b);
+        }
+        drop.flush();
+        drop.close();
+        in.close();
+
         out.println("<h1>Draw</h1>");
 
         out.println("<p><a href='form.html'>응모자</a></p>");
